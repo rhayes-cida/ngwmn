b99ee4b1b4a4fabeb74de6774e6c79e1d2e4aeb0
Roger Hayes <rhayes@usgs.gov>
2012-03-30 10:41:44 -0500

    OPEN - issue GWDP-206: Use Spring to configure dependencies http://internal.cida.usgs.gov/jira/browse/GWDP-206
    Switched to MyBatis (much cleaner than iBatis).
    Works.

--- src/main/java/gov/usgs/ngwmn/dm/dao/WellRegistryDAOTest.java
+++ src/main/java/gov/usgs/ngwmn/dm/dao/WellRegistryDAOTest.java
@@ -5,9 +5,7 @@ import static org.junit.Assert.*;
 import java.util.List;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -26,15 +24,23 @@ public class WellRegistryDAOTest {
 	}
 
 	@Test
-	public void testFindByKey() {
-		fail("Not yet implemented");
-	}
-
-	@Test
 	public void testSelectAll() {
 		List<WellRegistry> all = dao.selectAll();
 		assertNotNull("all", all);
 		assertFalse("empty", all.isEmpty());
+		
+		for (WellRegistry w : all) {
+			assertEquals("display flag", "1", w.getDisplayFlag());
+		}
+	}
+	
+	// IL EPA, P408223
+	@Test
+	public void testFindOne() {
+		WellRegistry w = dao.findByKey("IL EPA", "P408223");
+		assertNotNull("well",w);
+		assertEquals("agency", "IL EPA", w.getAgencyCd());
+		assertEquals("site", "P408223", w.getSiteNo());
 	}
 
 }
