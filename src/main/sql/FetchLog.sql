8df56cf76274e9ccb094f50cf5a58dc1f39f6794
Roger Hayes <rhayes@usgs.gov>
2012-03-30 17:07:23 -0500

    IN PROGRESS - issue GWDP-213: Populate Statistics http://internal.cida.usgs.gov/jira/browse/GWDP-213
    Made WellRegistry immutable.
    Cleaned up FetchLog.

--- src/main/sql/FetchLog.sql
+++ src/main/sql/FetchLog.sql
@@ -11,3 +11,19 @@ create table FETCH_LOG (
 	specifier varchar2(80),
 	primary key(fetchlog_id)
 );
+
+create sequence fetch_log_seq;
+
+create trigger fetch_log_trigger
+	before insert on FETCH_LOG
+	for each row
+	begin
+		select fetch_log_seq.nextval into :new.fetchlog_id from dual;
+	end;
+
+-- insert into fetch_log(status) values ('FAIL');
+-- select * from fetch_log;
+-- delete from fetch_log;
+
+
+
