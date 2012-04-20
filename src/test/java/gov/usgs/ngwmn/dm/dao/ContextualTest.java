9e76eaee03362422c7b4df200324132d8c733c24
R Hayes <rhayes@usgs.gov>
2012-04-20 11:10:41 -0500

    IN PROGRESS - issue GWDP-258: Collect all test data from tests into a single location and create necessary SQL http://internal.cida.usgs.gov/jira/browse/GWDP-258
    Added methods checkSiteExists and checkSiteIsVisble to test base class,
    use in FetchRecorderTest.

--- src/test/java/gov/usgs/ngwmn/dm/dao/ContextualTest.java
+++ src/test/java/gov/usgs/ngwmn/dm/dao/ContextualTest.java
@@ -1,11 +1,19 @@
 package gov.usgs.ngwmn.dm.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import gov.usgs.ngwmn.dm.cache.fs.FileCache;
+import gov.usgs.ngwmn.dm.spec.Specifier;
 
 import javax.naming.InitialContext;
+import javax.sql.DataSource;
 
 import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+import static org.junit.Assert.*;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.mock.jndi.SimpleNamingContextBuilder;
@@ -40,6 +48,54 @@ public abstract class ContextualTest {
 
 	public static void setBasedir(String basedir) {
 		ContextualTest.basedir = basedir;
+	}
+
+	protected void checkSiteExists(Specifier spec) throws Exception {
+		DataSource ds = ctx.getBean("dataSource", DataSource.class);
+		
+		Connection conn = ds.getConnection();
+		try {
+			PreparedStatement ps = conn.prepareStatement("SELECT count(*) from WELL_REGISTRY " +
+					"WHERE agency_cd = ? and site_no = ?");
+			ps.setString(1, spec.getAgencyID());
+			ps.setString(2, spec.getFeatureID());
+			ResultSet rs = ps.executeQuery();
+			int ct = 0;
+			while (rs.next()) {
+				ct = rs.getInt(1);
+			}
+			
+			if (ct != 1) {
+				throw new RuntimeException("Expected site not found: " + spec);
+			}
+		} finally {
+			conn.close();
+		}		
+	}
+
+	// low-level, should not depend on much besides the data
+	protected void checkSiteIsVisible(Specifier spec) throws Exception {
+		DataSource ds = ctx.getBean("dataSource", DataSource.class);
+		
+		Connection conn = ds.getConnection();
+		try {
+			PreparedStatement ps = conn.prepareStatement("SELECT count(*) from WELL_REGISTRY " +
+					"WHERE AGENCY_CD = ? and SITE_NO = ? " +
+					"and DISPLAY_FLAG = '1'");
+			ps.setString(1, spec.getAgencyID());
+			ps.setString(2, spec.getFeatureID());
+			ResultSet rs = ps.executeQuery();
+			int ct = 0;
+			while (rs.next()) {
+				ct = rs.getInt(1);
+			}
+			
+			if (ct != 1) {
+				throw new RuntimeException("Expected site not found or not visible: " + spec);
+			}
+		} finally {
+			conn.close();
+		}		
 	}
 
 }
