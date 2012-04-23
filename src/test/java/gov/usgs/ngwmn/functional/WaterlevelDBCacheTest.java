a4847bda7e37465620dbc7310f69073ff99da591
R Hayes <rhayes@usgs.gov>
2012-04-23 17:26:38 -0500

    IN PROGRESS - issue GWDP-257: Stabilize database cache http://internal.cida.usgs.gov/jira/browse/GWDP-257
    Configuration and tests for log and waterlevel.

--- src/test/java/gov/usgs/ngwmn/functional/WaterlevelDBCacheTest.java
+++ src/test/java/gov/usgs/ngwmn/functional/WaterlevelDBCacheTest.java
@@ -12,6 +12,7 @@ import java.util.Date;
 
 import javax.sql.DataSource;
 
+import gov.usgs.ngwmn.WellDataType;
 import gov.usgs.ngwmn.dm.cache.CacheInfo;
 import gov.usgs.ngwmn.dm.cache.qw.DatabaseXMLCache;
 import gov.usgs.ngwmn.dm.dao.ContextualTest;
@@ -29,11 +30,11 @@ import com.google.common.io.CountingOutputStream;
 import com.google.common.io.InputSupplier;
 import com.google.common.io.NullOutputStream;
 
-public class QWTableCacheTest extends ContextualTest {
+public class WaterlevelDBCacheTest extends ContextualTest {
 
-	private static final int SIZE = 17779;
-	private static final String TYPE = "QUALITY";
-	private static final String SITE = "394212075275101";
+	private static final int SIZE = 4102;
+	private static final String TYPE = WellDataType.WATERLEVEL.name();
+	private static final String SITE = "385748074511001";
 	private static final String AGENCY = "USGS";
 	private static final String filename = AGENCY+ "_" + SITE + "_" + TYPE;
 
@@ -41,7 +42,7 @@ public class QWTableCacheTest extends ContextualTest {
 	
 	@BeforeClass
 	public static void checkFile() throws Exception {
-		InputStream ris = QWTableCacheTest.class.getResourceAsStream("/sample-data/" + filename);
+		InputStream ris = WaterlevelDBCacheTest.class.getResourceAsStream("/sample-data/" + filename);
 		
 		CountingOutputStream cos = new CountingOutputStream(new NullOutputStream());
 		
@@ -99,8 +100,8 @@ public class QWTableCacheTest extends ContextualTest {
 		long ct = ByteStreams.copy(inp, os);
 		os.close();
 		
-		assertTrue("got bytes", ct > 0);
-		assertEquals("byte count", SIZE, ct);
+		assertTrue("expect got some bytes", ct > 0);
+		assertEquals("expect byte count has not changed", SIZE, ct);
 	}
 
 	private Specifier makeSpecifier() {
@@ -124,7 +125,7 @@ public class QWTableCacheTest extends ContextualTest {
 		ByteStreams.copy(iss, dest);
 		
 		// Contents may have expanded in shipping, due to pretty printing
-		assertTrue("byte count", dest.size() >= SIZE);
+		assertTrue("expect byte count has not shrunk", dest.size() >= SIZE);
 	}
 
 	// @Test
@@ -138,7 +139,7 @@ public class QWTableCacheTest extends ContextualTest {
 		
 		ByteStreams.copy(iss, System.out);
 		
-		assertTrue("still alive", true);
+		assertTrue("expect test method reaches end", true);
 	}
 
 	@Test
@@ -147,7 +148,7 @@ public class QWTableCacheTest extends ContextualTest {
 		
 		boolean e = victim.contains(spec);
 		
-		assertTrue("exists", e);
+		assertTrue("expect cache exists", e);
 	}
 	
 	@Test
@@ -158,10 +159,10 @@ public class QWTableCacheTest extends ContextualTest {
 		
 		// Contents may have expanded in shipping, due to pretty printing
 		// assertEquals(SIZE, info.getLength());
-		assertTrue("size", info.getLength() >= SIZE);
-		assertTrue(info.isExists());
+		assertTrue("expect size no smaller than inpout file", info.getLength() >= SIZE);
+		assertTrue("expect cache exists",info.isExists());
 		
-		assertTrue("created before now", info.getCreated().before(new Date()));
-		assertFalse("created after modified", info.getCreated().after(info.getModified()));
+		assertTrue("expect created before now", info.getCreated().before(new Date()));
+		assertFalse("expect create time after modify time", info.getCreated().after(info.getModified()));
 	}
 }
