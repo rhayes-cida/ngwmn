0e324fe884c0fcc5f0bbeb25867065ff5ec7f123
R Hayes <rhayes@usgs.gov>
2012-05-18 17:14:58 -0500

    OPEN - issue GWDP-282: Data quality evaluator http://internal.cida.usgs.gov/jira/browse/GWDP-282
    Better waterlevel XQuery expression.

--- src/main/sql/WaterlevelQA.sql
+++ src/main/sql/WaterlevelQA.sql
@@ -29,3 +29,28 @@ columns
 "VAL" varchar(40) path 'val',
 "UNITS" varchar(40) path 'units'
 ) xq;
+
+
+-- ahah! don't need to construct intermediate XML fragment
+select 
+	qc.waterlevel_cache_id,
+	xq.*
+from 
+	waterlevel_cache qc,
+	
+XMLTable(
+XMLNAMESPACES(
+  'http://www.wron.net.au/waterml2' AS "wml2",
+  'http://www.opengis.net/om/2.0' AS "om",
+  'http://www.opengis.net/swe/2.0' AS "swe"),
+'for $r in //wml2:WaterMonitoringObservation/om:result/wml2:TimeSeries/wml2:element
+let 
+$p := $r/wml2:TimeValuePair
+return $p
+'
+passing qc.xml
+columns 
+"DT" varchar(40) path 'wml2:time',
+"VAL" varchar(40) path 'wml2:value/swe:Quantity/swe:value',
+"UNITS" varchar(40) path 'wml2:value/swe:Quantity/swe:uom/@code'
+) xq;
