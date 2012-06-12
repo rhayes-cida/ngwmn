740f8a5c13d169c2167873618274a8149a776d41
R Hayes <rhayes@usgs.gov>
2012-06-12 15:45:05 -0500

    OPEN - issue GWDP-311: Multi-site download not using cached data http://internal.cida.usgs.gov/jira/browse/GWDP-311
    Fixed.

--- src/main/java/gov/usgs/ngwmn/dm/cache/Retriever.java
+++ src/main/java/gov/usgs/ngwmn/dm/cache/Retriever.java
@@ -1,26 +1,55 @@
 package gov.usgs.ngwmn.dm.cache;
 
+import gov.usgs.ngwmn.WellDataType;
 import gov.usgs.ngwmn.dm.DataFetcher;
 import gov.usgs.ngwmn.dm.io.Pipeline;
 import gov.usgs.ngwmn.dm.spec.Specifier;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSortedMap;
 
 public class Retriever implements DataFetcher {
-	private Cache cache;
+	protected final Logger logger = LoggerFactory.getLogger(getClass());
+	private Map<WellDataType, Cache> caches;
 	
-	public Retriever(Cache c) {
-		this.cache = c;
+	public Retriever(Collection<Cache> cc) {
+		ImmutableMap.Builder<WellDataType,Cache> builder = ImmutableSortedMap.naturalOrder();		
+		for (Cache c : cc) {
+			builder.put(c.getDatatype(), c);
+		}
+		caches = builder.build();
+		
+		logger.debug("initialized, cache map = {}", caches);
+	}
+
+	public Cache getCache(WellDataType t) {
+		return caches.get(t);
 	}
 
 	@Override
 	public boolean configureInput(Specifier spec, Pipeline pipe)
 			throws IOException 
 	{
+		logger.debug("looking for {}", spec);
+		Cache cache = getCache(spec.getTypeID());
+		if (cache == null) {
+			logger.info("no cache for {}", spec);
+			return false;
+		}
+		
+		logger.debug("cache for {} is {}", spec, cache);
 		if (cache.contains(spec)) {
-			// pipe.getStatistics().setCalledBy(this.getClass());
+			logger.info("found cache entry for {}", spec);
 			return cache.fetchWellData(spec, pipe); 
 		}
+		logger.info("cache {} has no entry for {}", cache, spec);
 		return false;
 	}
 	
