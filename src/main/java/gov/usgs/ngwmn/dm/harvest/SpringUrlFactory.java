0d55999ea1206d400b7d00dda4b1fbbe78d77883
R Hayes <rhayes@usgs.gov>
2012-06-25 11:14:35 -0500

    RESOLVED - issue GWDP-347: URL hardcoded deep inside properties file http://internal.cida.usgs.gov/jira/browse/GWDP-347
    Add safety check

---
+++ src/main/java/gov/usgs/ngwmn/dm/harvest/SpringUrlFactory.java
@@ -0,0 +1,43 @@
+package gov.usgs.ngwmn.dm.harvest;
+
+import gov.usgs.ngwmn.WellDataType;
+import gov.usgs.ngwmn.dm.spec.Specifier;
+
+import java.util.Properties;
+
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.common.TemplateParserContext;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+
+public class SpringUrlFactory {
+
+	private Properties myProps;
+	
+	public void setMapping(Properties pp) {
+		myProps = pp;
+		for (WellDataType type : WellDataType.values()) {
+			String url = (String) pp.get( type.name() );
+			if (url == null) {
+				throw new Error("Missing URL mapping for data type " + type);
+			}
+		}
+	}
+	
+	public String resolve(WellDataType t, String agency, String site) {
+		String exp = (String) myProps.get(t.name());
+		
+		ExpressionParser parser = new SpelExpressionParser();
+
+		StandardEvaluationContext context = new StandardEvaluationContext();
+		context.setVariable("agencyId", agency);
+		context.setVariable("featureId", site);
+		
+		return parser.parseExpression(exp,
+				new TemplateParserContext("_{","}")).getValue(context, String.class);
+	}
+	
+	public String makeUrl(Specifier spec) {
+		return resolve(spec.getTypeID(), spec.getAgencyID(), spec.getFeatureID());
+	}
+}
