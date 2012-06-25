0f21a203f6645ea20de506566990172c67647d8e
R Hayes <rhayes@usgs.gov>
2012-06-25 18:24:01 -0500

    Test for prefetch configuration (and override).

---
+++ src/test/java/gov/usgs/ngwmn/dm/prefetch/PrefetchConfigurationTest.java
@@ -0,0 +1,60 @@
+package gov.usgs.ngwmn.dm.prefetch;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class PrefetchConfigurationTest {
+
+	@Before
+	public void setCocoon() {
+		System.setProperty("ngwmn_cocoon", "http://localhost:8080/cocoon");
+
+	}
+	
+	@Test
+	public void test_custom_values() {
+		System.setProperty("ngwmn_prefetch_count_limit", "401");
+		System.setProperty("ngwmn_prefetch_ms_limit", "19181921");
+		System.setProperty("ngwmn_prefetch_start_hour", "03");
+
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContextTest.xml");
+		
+		Prefetcher pef = ctx.getBean("Prefetcher", Prefetcher.class);
+		assertEquals("count", 401, pef.getFetchLimit());
+		assertEquals("time", 19181921L, pef.getTimeLimit().longValue());
+		
+		Object sked = ctx.getBean("scheduler");
+		assertNotNull("scheduler", sked);
+	}
+
+	@Test
+	public void test_default_values() {
+		System.clearProperty("ngwmn_prefetch_count_limit");
+		System.clearProperty("ngwmn_prefetch_ms_limit");
+		System.clearProperty("ngwmn_prefetch_start_hour");
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContextTest.xml");
+		
+		Prefetcher pef = ctx.getBean("Prefetcher", Prefetcher.class);
+		assertEquals("count", 400, pef.getFetchLimit());
+		assertEquals("time", 18000000L, pef.getTimeLimit().longValue());
+		
+		Object sked = ctx.getBean("scheduler");
+		assertNotNull("scheduler", sked);
+	}
+
+	@Test(expected=BeanCreationException.class)
+	public void test_bad_hour() {
+		System.setProperty("ngwmn_prefetch_start_hour", "bad");
+
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContextTest.xml");
+		
+		fail("should have thrown exception in context load");
+	}
+
+
+}
