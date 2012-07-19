93d89fe027edc8757e532aa524a89a1dbe7569a5
R Hayes <rhayes@usgs.gov>
2012-07-19 13:54:00 -0500

    OPEN - issue GWDP-326: Add Additonal 'General' File to the Export http://internal.cida.usgs.gov/jira/browse/GWDP-326
    Implemented data provider (with default column names).

--- src/main/java/gov/usgs/ngwmn/WellDataType.java
+++ src/main/java/gov/usgs/ngwmn/WellDataType.java
@@ -7,24 +7,34 @@ public enum WellDataType {
 	CONSTRUCTION("text/xml", "xml", LOG,  "construction"),
 	WATERLEVEL  ("text/xml", "xml", null, "TimeValuePair"),
 	QUALITY     ("text/xml", "xml", null, "Result"),
-	ALL("application/zip", "zip", null, "n/a"); // TODO ALL asdf
+	REGISTRY	("text/xml", "xml", null, "row", false),
+	ALL			("application/zip", "zip", null, "n/a", false); // TODO ALL asdf
 
 	
 	public final String contentType;
 	public final String suffix;
 	public final String rowElementName;
 	public final WellDataType aliasFor;
+	public final boolean cachable;
 	
 
 	private WellDataType(String contentType, String suffix, WellDataType alias, String rowElementName) {
-		
+		this(contentType, suffix, alias, rowElementName, true);		
+	}
+	
+	private WellDataType(String contentType, String suffix, WellDataType alias, String rowElementName, boolean cachable) {
 		this.contentType        = contentType;
 		this.suffix             = suffix;
 		this.rowElementName     = rowElementName;
 		this.aliasFor           = (alias == null) ? this : alias;
+		this.cachable 			= cachable;
 	}
-	
+
 	public String makeFilename(String wellName) {
 		return wellName + "_" + this.name() + "." + this.suffix;
+	}
+
+	public boolean isCachable() {
+		return cachable;
 	}
 }
---
+++ src/main/java/gov/usgs/ngwmn/dm/cache/NullCache.java
@@ -0,0 +1,52 @@
+package gov.usgs.ngwmn.dm.cache;
+
+import gov.usgs.ngwmn.WellDataType;
+import gov.usgs.ngwmn.dm.io.Pipeline;
+import gov.usgs.ngwmn.dm.spec.Specifier;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.google.common.io.NullOutputStream;
+
+public class NullCache implements Cache {
+
+	private WellDataType wdt;
+	
+	public NullCache(WellDataType wdt) {
+		this.wdt = wdt;
+	}
+
+	@Override
+	public OutputStream destination(Specifier well) throws IOException {
+		return new NullOutputStream();
+	}
+
+	@Override
+	public boolean fetchWellData(Specifier spec, Pipeline pipe)
+			throws IOException {
+		return false;
+	}
+
+	@Override
+	public boolean contains(Specifier spec) {
+		return false;
+	}
+
+	@Override
+	public CacheInfo getInfo(Specifier spec) {
+		return null;
+	}
+
+	@Override
+	public WellDataType getDatatype() {
+		return wdt;
+	}
+
+	@Override
+	public InputStream retrieve(String id) throws IOException {
+		throw new RuntimeException("the retrieve method is not implemented and should not be called");
+	}
+
+}
