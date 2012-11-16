e82506643bc2bcada673e3ca64978993d69271c5
R Hayes <rhayes@usgs.gov>
2012-11-16 10:57:07 -0600

    IN PROGRESS - issue GWDP-462: Add measured parameter and mediated water level to CSV file http://internal.cida.usgs.gov/jira/browse/GWDP-462
    Add test for escaped commas.

--- src/test/java/gov/usgs/ngwmn/dm/io/transform/DirectCSVOutputStreamTest.java
+++ src/test/java/gov/usgs/ngwmn/dm/io/transform/DirectCSVOutputStreamTest.java
@@ -2,6 +2,7 @@ package gov.usgs.ngwmn.dm.io.transform;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -13,7 +14,7 @@ import org.junit.Test;
 public class DirectCSVOutputStreamTest {
 
 	@Test
-	public void test() throws Exception {
+	public void testSample() throws Exception {
 		ByteArrayOutputStream bos = new ByteArrayOutputStream();
 		DirectCSVOutputStream victim = new DirectCSVOutputStream(bos);
 		
@@ -34,6 +35,82 @@ public class DirectCSVOutputStreamTest {
 		assertTrue("has value", result.contains("up,ft,14.04"));
 	}
 
+	@Test
+	public void testSkipHeaders() throws Exception {
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		DirectCSVOutputStream victim = new DirectCSVOutputStream(bos);
+		
+		victim.setExecutor(Executors.newSingleThreadExecutor());
+		victim.setAgency("USGS");
+		victim.setSite("20515416303801");
+		victim.setElevation(99.999);
+		victim.setWrittenHeaders(true);
+		InputStream tis = getClass().getResourceAsStream("/sample-data/USGS_20515416303801_WATERLEVEL_ABBREV.xml");
+		
+		copy(tis,victim);
+		
+		victim.close();
+		
+		String result = bos.toString();
+		
+		assertFalse("has header", result.contains("Mediated Value"));
+		assertTrue("has timestamp", result.contains("1983-08-25"));
+		assertTrue("has value", result.contains("up,ft,14.04"));
+	}
+
+	@Test
+	public void testEmpty() throws Exception {
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		DirectCSVOutputStream victim = new DirectCSVOutputStream(bos);
+		
+		victim.setExecutor(Executors.newSingleThreadExecutor());
+		victim.setAgency("USGS");
+		victim.setSite("20515416303801");
+		victim.setElevation(99.999);
+		victim.setWrittenHeaders(false);
+		
+		byte[] emptyBuf = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<nothing/>\n".getBytes();
+		InputStream tis = new ByteArrayInputStream(emptyBuf);
+		
+		copy(tis,victim);
+		
+		victim.close();
+		
+		String result = bos.toString();
+		
+		assertTrue("has header", result.contains("Mediated Value"));
+		assertFalse("has timestamp", result.contains("1983-08-25"));
+		assertFalse("has value", result.contains("up,ft,14.04"));
+		
+	}
+	
+	@Test
+	public void testComma() throws Exception {
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		DirectCSVOutputStream victim = new DirectCSVOutputStream(bos);
+		
+		victim.setExecutor(Executors.newSingleThreadExecutor());
+		victim.setAgency("USGS");
+		victim.setSite("20515416303801");
+		victim.setElevation(99.999);
+		victim.setWrittenHeaders(false);
+		
+		InputStream tis = getClass().getResourceAsStream("/sample-data/USGS_SAMPLE_WATERLEVEL_COMMAS.xml");
+
+		copy(tis,victim);
+		
+		victim.close();
+		
+		String result = bos.toString();
+		
+		System.out.println(result);
+		
+		assertTrue("has header", result.contains("Mediated Value"));
+		assertFalse("has raw input", result.contains("Comment, with embedded commas, and some \"quoted text\" as well"));
+		assertTrue("has escaped commas", result.contains("\"Comment, with embedded commas, and some \"\"quoted text\"\" as well\""));
+		
+	}
+	
 	private void copy(InputStream is, OutputStream os) throws IOException {
 		while (true) {
 			int c = is.read();
