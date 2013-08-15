9f939f60627593b625d51526d3ad7fb9d33be5d6
R Hayes <rhayes@usgs.gov>
2013-08-15 12:17:28 -0500

    SOS GetFeatureOfInterest works, except: * id filter does not work, due to geoserver configuration problems * bounding box is untested

--- src/main/java/gov/usgs/ngwmn/sos/SOSService.java
+++ src/main/java/gov/usgs/ngwmn/sos/SOSService.java
@@ -1,8 +1,11 @@
 package gov.usgs.ngwmn.sos;
 
 import gov.usgs.ngwmn.NotImplementedException;
+import gov.usgs.ngwmn.dm.io.TeeInputStream;
 import gov.usgs.ngwmn.dm.io.transform.XSLHelper;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -19,6 +22,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.google.common.io.CountingInputStream;
+import com.google.common.io.CountingOutputStream;
 
 @Controller
 public class SOSService {
@@ -45,23 +51,57 @@ public class SOSService {
 	
 	// GetFeatureOfInterest
 	// implement on the back of geoserver
-	// two forms of input: featureId or bounding box
+	// two forms of filter: featureId or bounding box
 	// example URL $BASE?REQUEST=GetFeatureOfInterest&VERSION=2.0.0&SERVICE=SOS&featureOfInterest=ab.mon.45
-	@RequestMapping(params={"REQUEST=GetFeatureOfInterest","featureId"})
+	@RequestMapping(params={"REQUEST=GetFeatureOfInterest"})
 	public void getFOI_byId(
-			@RequestParam String featureId,
+			@RequestParam(required=false) String featureId,
+			@RequestParam(required=false) String spatialFilter,
 			HttpServletResponse response
 			)
 		throws Exception
 	{
 		GeoserverFeatureSource featureSource = new GeoserverFeatureSource();
 		
-		logger.info("GetFeatureOfInterest,featureId={}", featureId);
+		logger.info("GetFeatureOfInterest");
 		
-		// variable parameters
-		// TODO fix param name and perhaps value
-		featureSource.addParameter("featureOfInterest", featureId);
-
+		// optional filters
+		int filterCt = 0;
+		
+		if (featureId != null) {
+			// TODO fix param name and perhaps value
+			featureSource.addParameter("featureID", featureId);
+			logger.debug("Added filter fid={}", featureId);
+			filterCt++;
+		}
+		
+		if (spatialFilter != null) {
+			String[] part = spatialFilter.split(",");
+			if ( ! "om:featureOfInterest/*/sams:shape".equals(part[0]) ) {
+				throw new RuntimeException("bad filter");
+			}
+			for (int i = 1; i <= 4; i++) {
+				// just for validation
+				Double.parseDouble(part[i]);
+			}
+		
+			// extra params for GeoServer WFS request, example:
+			// use the original input strings for fidelity
+			String cql_filter = MessageFormat.format("(BBOX(GEOM,{1},{2},{3,{4}))",
+					part[1],part[2], part[3], part[4]);
+			String srsName = "EPSG:4326";
+				
+			featureSource.addParameter("srsName", srsName);
+			featureSource.addParameter("CQL_FILTER", cql_filter);
+			
+			logger.debug("added spatial filter {} in srs {}", cql_filter, srsName);
+			filterCt++;
+		}
+		
+		if (filterCt == 0) {
+			logger.warn("No filters for WFS request, may get lots of data");
+		}
+		
 		try {
 			InputStream is = featureSource.getStream();
 			
@@ -69,66 +109,55 @@ public class SOSService {
 			OutputStream os = response.getOutputStream();
 
 			// copy from stream to response, filtering through xsl transform
-			copyThroughTransform(is,os, "/gov/usgs/ngwmn/hydrograph.xsl");
+			copyThroughTransform(is,os, "/gov/usgs/ngwmn/geoserver-2-sos.xsl");
+			logger.debug("done");
+		}
+		catch (Exception e) {
+			logger.warn("Problem", e);
+			throw e;
 		}
 		finally {
 			featureSource.close();
 		}
 	}
 	
-	// example URL $BASE?REQUEST=GetFeatureOfInterest&VERSION=2.0.0&SERVICE=SOS&spatialFilter=om:featureOfInterest/*/sams:shape,-116,50.5,-114.3,51.6&namespaces=xmlns(sams,http://www.opengis.net/samplingSpatial/2.0),xmlns(om,http://www.opengis.net/om/2.0)
-	@RequestMapping(params={"REQUEST=GetFeatureOfInterest","spatialFilter"})
-	public void getFOI_bySpatialFilter(
-			@RequestParam String spatialFilter,
-			HttpServletResponse response
-			)  throws Exception
-	{
-		String[] part = spatialFilter.split(",");
-		if ( ! "om:featureOfInterest/*/sams:shape".equals(part[0]) ) {
-			throw new RuntimeException("bad filter");
-		}
-		for (int i = 1; i <= 4; i++) {
-			// just for validation
-			Double.parseDouble(part[i]);
-		}
-		
-		// extra params for GeoServer WFS request, example:
-		// use the original input strings for fidelity
-		String cql_filter = MessageFormat.format("(BBOX(GEOM,{1},{2},{3,{4}))",
-				part[1],part[2], part[3], part[4]);
-		String srsName = "EPSG:4326";
-		
-		GeoserverFeatureSource featureSource = new GeoserverFeatureSource();
-		
-		// variable parameters
-		featureSource.addParameter("srsName", srsName);
-		featureSource.addParameter("CQL_FILTER", cql_filter);
-
-		try {
-			InputStream is = featureSource.getStream();
-			
-			response.setContentType("text/xml");
-			OutputStream os = response.getOutputStream();
-
-			// copy from stream to response, filtering through xsl transform
-			copyThroughTransform(is,os, "/gov/usgs/ngwmn/hydrograph.xsl");
-		}
-		finally {
-			featureSource.close();
-		}
-	}
-
 	public static void copyThroughTransform(InputStream is, OutputStream os,
 			String xformName) throws IOException, TransformerException {
 		XSLHelper xslHelper = new XSLHelper();
 		xslHelper.setTransform(xformName);
+		
+		CountingInputStream countingIs = null;
+		TeeInputStream teeIs = null;
+		CountingOutputStream countingOs = null;
+		
+		if (logger.isDebugEnabled()) {
+			countingIs = new CountingInputStream(is);
+			is = countingIs;
+			
+			countingOs = new CountingOutputStream(os);
+			os = countingOs;
+		}
+		
+		if (logger.isTraceEnabled()) {
+			File tOut = File.createTempFile("geoserver",".xml");
+			logger.info("Saving a copy of geo-output to {}", tOut);
+			FileOutputStream fos = new FileOutputStream(tOut);
+			teeIs = new TeeInputStream(is, fos, true);
+			is = teeIs;
+		}
 		
 		Transformer t = xslHelper.getTemplates().newTransformer();
 		StreamResult result = new StreamResult(os);
 		StreamSource source = new StreamSource(is);	
 
 		t.transform(source, result);
-
+		
+		if (countingIs != null) {
+			logger.debug("Processed {} bytes of input", countingIs.getCount());
+		}
+		if (countingOs != null) {
+			logger.debug("Got {} bytes of output", countingOs.getCount());
+		}
 	}
 	
 	
