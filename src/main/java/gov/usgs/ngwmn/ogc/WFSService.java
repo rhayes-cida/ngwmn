c3695f822b753ac8c765a00964d408e9c31aed5a
R Hayes <rhayes@usgs.gov>
2013-08-22 11:28:03 -0500

    IMPLEMENTING - issue GWDP-705: Create Pipeline to Transform Well Characteristic data to GWML https://internal.cida.usgs.gov/jira/browse/GWDP-705
    Rudimentary implementation, will minimal output.

--- src/main/java/gov/usgs/ngwmn/ogc/WFSService.java
+++ src/main/java/gov/usgs/ngwmn/ogc/WFSService.java
@@ -1,20 +1,11 @@
 package gov.usgs.ngwmn.ogc;
 
 import gov.usgs.ngwmn.NotImplementedException;
-import gov.usgs.ngwmn.dm.io.TeeInputStream;
-import gov.usgs.ngwmn.dm.io.transform.XSLHelper;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 import javax.servlet.http.HttpServletResponse;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,18 +13,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.google.common.io.CountingInputStream;
-import com.google.common.io.CountingOutputStream;
 
 @Controller
-public class WFSService {
+public class WFSService extends OGCService {
 
 	public static final String FEATURE_PREFIX = "VW_GWDP_GEOSERVER";
 	
 	static private Logger logger = LoggerFactory.getLogger(WFSService.class);
 
-	private String geoserverURL;
-	
 	@RequestMapping(params={"REQUEST=GetCapabilities"})
 	public void getCapabilities(
 			OutputStream out
@@ -78,54 +65,6 @@ public class WFSService {
 		finally {
 			featureSource.close();
 		}
-	}
-	
-	public static void copyThroughTransform(InputStream is, OutputStream os,
-			String xformName) throws IOException, TransformerException {
-		XSLHelper xslHelper = new XSLHelper();
-		xslHelper.setTransform(xformName);
-		
-		CountingInputStream countingIs = null;
-		TeeInputStream teeIs = null;
-		CountingOutputStream countingOs = null;
-		
-		if (logger.isDebugEnabled()) {
-			countingIs = new CountingInputStream(is);
-			is = countingIs;
-			
-			countingOs = new CountingOutputStream(os);
-			os = countingOs;
-		}
-		
-		if (logger.isTraceEnabled()) {
-			File tOut = File.createTempFile("geoserver",".xml");
-			logger.info("Saving a copy of geo-output to {}", tOut);
-			FileOutputStream fos = new FileOutputStream(tOut);
-			teeIs = new TeeInputStream(is, fos, true);
-			is = teeIs;
-		}
-		
-		Transformer t = xslHelper.getTemplates().newTransformer();
-		StreamResult result = new StreamResult(os);
-		StreamSource source = new StreamSource(is);	
-
-		t.transform(source, result);
-		
-		if (countingIs != null) {
-			logger.debug("Processed {} bytes of input", countingIs.getCount());
-		}
-		if (countingOs != null) {
-			logger.debug("Got {} bytes of output", countingOs.getCount());
-		}
-	}
-
-	public String getGeoserverURL() {
-		return geoserverURL;
-	}
-
-	public void setGeoserverURL(String gsURL) {
-		this.geoserverURL = gsURL;
-		logger.info("Will use geoserver URL {}", this.geoserverURL);
 	}
 	
 }
---
+++ src/main/resources/gov/usgs/ngwmn/geoserver-2-gwml.xsl
@@ -0,0 +1,69 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!--  translate the ngwmn: namespaced output of our geoserver NGWMN layer to GWML -->
+<xsl:stylesheet version="2.0" 
+	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+
+	xmlns:xsl="http://www.w3.org/1999/XSL/Transform" 
+	
+	xmlns:wfs="http://www.opengis.net/wfs" 
+	xmlns:ngwmn="gov.usgs.cida.ngwmn"
+	xmlns:gml="http://www.opengis.net/gml" 
+	xmlns:gwml="http://www.nrcan.gc.ca/xml/gwml/1"
+	
+	xmlns:sos="http://www.opengis.net/sos/2.0" 
+	xmlns:gml32="http://www.opengis.net/gml/3.2"
+	xmlns:swes="http://www.opengis.net/swes/2.0" 
+	xmlns:om="http://www.opengis.net/om/2.0"
+	xmlns:sf="http://www.opengis.net/sampling/2.0" 
+	xmlns:xlink="http://www.w3.org/1999/xlink"
+	xmlns:sams="http://www.opengis.net/samplingSpatial/2.0" 
+	xmlns:x="urn:gin:include:1.0"
+		
+	exclude-result-prefixes="ngwmn "
+	
+	
+	xmlns:xalan="http://xml.apache.org/xalan"
+	
+	>
+	<xsl:output method="xml" version="1.0" encoding="UTF-8" indent="yes" xalan:indent-amount="2"/>
+	
+	<xsl:template match="/wfs:FeatureCollection">
+	<xsl:text >
+</xsl:text>
+	<wfs:FeatureCollection>
+		<!-- metadata can go here -->
+		<xsl:apply-templates select=".//gml:featureMember"/>
+	</wfs:FeatureCollection>
+	</xsl:template>
+	
+	<xsl:template match="gml:featureMember">
+		<gml:featureMember>
+			<xsl:apply-templates select="ngwmn:VW_GWDP_GEOSERVER"/>
+		</gml:featureMember>
+	</xsl:template>
+	
+	<xsl:template match="ngwmn:SITE_NAME">
+		<gml:description><xsl:value-of select="text()"/></gml:description>
+	</xsl:template>
+	
+	<xsl:template match="gml:Point">
+		<gml:Point>
+			<gml:pos srsName="{@srsName}"><xsl:value-of select="translate(gml:coordinates/text(),',',' ')"/></gml:pos>
+		</gml:Point>
+	</xsl:template>
+	
+	<xsl:template match="ngwmn:VW_GWDP_GEOSERVER">
+		<gwml:WaterWell gml:id="{@fid}">
+		<xsl:choose>
+			<xsl:when test="ngwmn:SITE_NAME">
+					<gml:description><xsl:value-of select="ngwmn:SITE_NAME"/></gml:description>
+			</xsl:when>
+			<xsl:otherwise>
+					<gml:description><xsl:value-of select="ngwmn:MY_SITEID"/></gml:description>
+			</xsl:otherwise>
+		</xsl:choose>
+		<!-- lots more stuff -->
+		</gwml:WaterWell>
+	</xsl:template>
+	
+	</xsl:stylesheet>
